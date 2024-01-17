2d17cbdd9a2c27bd87166448e49f8765d3d02bc9
Pavel Davydov <Davydov.works@gmail.com>
2024-01-17 13:16:01 -0500

    add taskEasy

---
+++ src/main/java/HomeTask/easy/ConsoleInput.java
@@ -0,0 +1,19 @@
+package HomeTask.easy;
+
+import java.util.Scanner;
+
+public class ConsoleInput {
+    public static void main(String[] args) {
+        System.out.println("Введите число и степень");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+        int result = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            result *= a;
+        }
+        System.out.println(a  + " В степени " + n + " равно " + result );
+    }
+}
