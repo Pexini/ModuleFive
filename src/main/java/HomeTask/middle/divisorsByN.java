c58924a9e3b1fa792d059ac00eb40eddf0b1469b
Pavel Davydov <Davydov.works@gmail.com>
2024-01-17 16:25:15 -0500

    add 3taskMiddle

---
+++ src/main/java/HomeTask/middle/divisorsByN.java
@@ -0,0 +1,24 @@
+package HomeTask.middle;
+
+import java.util.Scanner;
+
+public class divisorsByN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите значение N: ");
+        int n = scanner.nextInt();
+
+        if (n < 0 || n > 100) {
+            System.out.println("Значение N доблжно быть больше 100 ");
+            return;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                System.out.println("Дедители " + i);
+
+
+            }
+        }
+    }
+}
+
