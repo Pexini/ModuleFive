aeaad250a1d0d64adfb6a0033c7d2a90661abab1
Pavel Davydov <Davydov.works@gmail.com>
2024-01-18 14:06:55 -0500

    add 6taskMiddle

--- src/main/java/HomeTask/middle/multiplicatioTable.java
+++ src/main/java/HomeTask/middle/multiplicatioTable.java
@@ -5,22 +5,26 @@ import java.util.Scanner;
 
 public class multiplicatioTable {
     public static void main(String[] args) {
-        int a = Math.abs(new Random().nextInt()) % 9 + 1;
-        int b = Math.abs(new Random().nextInt()) % 9 + 1;
-        int c = a * b;
-        int userNum = 0;
-        System.out.println(" Ответь на вопрос сколько будет " + a + " умножить на " + b + " ?");
 
-        while (userNum != c) {
+        int userNum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int a = Math.abs(new Random().nextInt()) % 9 + 1;
+            int b = Math.abs(new Random().nextInt()) % 9 + 1;
+            int c = a * b;
+
+            System.out.println(" Ответь на вопрос сколько будет " + a + " * " + b + " ?");
+
             Scanner scanner = new Scanner(System.in);
             userNum = scanner.nextInt();
 
             if (userNum == c) {
-                System.out.println("правильно " + a + " умножить на " + b + " равно " + c);
+                System.out.println("Правильно : " + c);
             } else {
-                System.out.println("неправильно " + a + " умножить на " + b + " равно " + c);
+                System.out.println("Hеправильно: " + a + " умножить на " + b + " = " + c);
             }
 
         }
     }
 }
+
