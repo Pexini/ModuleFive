25144c08e1b5f8c0e5fa0b18246677195f02dd13
Pavel Davydov <Davydov.works@gmail.com>
2024-01-18 14:20:37 -0500

    add FirstHard

---
+++ src/main/java/HomeTask/hard/Fibonachi.java
@@ -0,0 +1,16 @@
+package HomeTask.hard;
+
+import java.util.Scanner;
+
+public class Fibonachi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите количество элементов в ряде Фибоначчи: ");
+        int n = scanner.nextInt();
+
+
+    }
+}
+
+
