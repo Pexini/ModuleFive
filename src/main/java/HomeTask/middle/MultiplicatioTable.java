8b2467e9cfab8850a6b8ed3683760fe3c67524a4
Pavel Davydov <Davydov.works@gmail.com>
2024-01-20 13:01:46 -0500

    reformat MultiplicatioTable

--- src/main/java/HomeTask/middle/MultiplicatioTable.java
+++ src/main/java/HomeTask/middle/MultiplicatioTable.java
@@ -7,21 +7,31 @@ public class MultiplicatioTable {
     public static void main(String[] args) {
 
         int userNum = 0;
+        int patternsRight = 0;
+        int patternsWrong = 0;
 
         for (int i = 0; i < 15; i++) {
-            int a = Math.abs(new Random().nextInt()) % 9 + 1;
-            int b = Math.abs(new Random().nextInt()) % 9 + 1;
+            int a = Math.abs(new Random().nextInt(9)) + 1;
+            int b = Math.abs(new Random().nextInt(9)) + 1;
             int c = a * b;
+
 
             System.out.println(" Ответь на вопрос сколько будет " + a + " * " + b + " ?");
 
             Scanner scanner = new Scanner(System.in);
             userNum = scanner.nextInt();
 
+
             if (userNum == c) {
-                System.out.println("Правильно : " + c);
+                patternsRight++;
+               // System.out.println("Пример № " + patterns + " Правильно : " + c);
             } else {
-                System.out.println("Hеправильно: " + a + " умножить на " + b + " = " + c);
+                patternsWrong++;
+             //   System.out.println("Пример № " + patterns + " Hеправильно: " + a + " умножить на " + b + " = " + c);
+            }
+            if ((patternsRight + patternsWrong) % 5 == 0) {
+                System.out.println( "Ваш результат : " + " Правильные ответы = " +  patternsRight + "; Неправильные ответы =  " + patternsWrong );
+                break;
             }
 
         }
