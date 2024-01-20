11cfcd62d9482e177dbe9df783ca104efacc3f85
Pavel Davydov <Davydov.works@gmail.com>
2024-01-20 12:31:26 -0500

    reformat taskModule

--- src/main/java/HomeTask/middle/Module.java
+++ src/main/java/HomeTask/middle/Module.java
@@ -5,11 +5,12 @@ public class Module {
         int n = 3;
         double[] array = {3.2, -1.3, 7.3};
 
-        double maxValue = Math.abs(array[0]);
+        double maxValue = 0;
 
         for (int i = 1; i < n; i++) {
             double value = Math.abs(array[i]);
-            if (value > maxValue) {
+
+            if (i == 0 || value > maxValue) {
                 maxValue = value;
             }
         }
