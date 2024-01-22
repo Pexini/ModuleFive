bb269ebcbe6ce3dc9e0cd2b6110f9d5018c33fca
Pavel Davydov <Davydov.works@gmail.com>
2024-01-22 14:25:43 -0500

    reformat MultiplicatioTable

--- src/main/java/HomeTask/middle/MultiplicatioTable.java
+++ src/main/java/HomeTask/middle/MultiplicatioTable.java
@@ -9,33 +9,64 @@ public class MultiplicatioTable {
         int userNum = 0;
         int answerRight = 0;
         int answerWrong = 0;
+        int extraAnswer = 0;
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i < 15; i++) {
-            int a = Math.abs(new Random().nextInt(9)) + 1;
-            int b = Math.abs(new Random().nextInt(9)) + 1;
+        for (int i = 0; i < 5; i++) {
+            int a = Math.abs(random.nextInt(9)) + 1;
+            int b = Math.abs(random.nextInt(9)) + 1;
             int c = a * b;
 
-            System.out.println( );
+            System.out.println();
             System.out.println(" Решите пример № " + i + "   " + a + " * " + b + " ?");
 
-            Scanner scanner = new Scanner(System.in);
+
             userNum = scanner.nextInt();
 
 
             if (userNum == c) {
                 answerRight++;
                 System.out.println("Пример № " + i + "\n Правильно : " + c);
+
             } else {
                 answerWrong++;
                 System.out.println("Пример № " + i + " \n Hеправильно: " + a + " умножить на " + b + " = " + c);
+                System.out.println();
 
+
+                for (int w = 0; w < 1; w++) {
+                    int newA = Math.abs(random.nextInt(9)) + 1;
+                    int newB = Math.abs(random.nextInt(9)) + 1;
+                    int newC = newA * newB;
+                    extraAnswer++;
+
+                    System.out.println("Дополнительный вопрос: " + newA + " * " + newB + " ?");
+                    userNum = scanner.nextInt();
+
+                    while (userNum != newC) {
+                        extraAnswer++;
+                        System.out.println("Дополнительный вопрос: " + newA + " * " + newB + " ?");
+                        userNum = scanner.nextInt();
+                    }
+                    if (userNum == newC)
+                        extraAnswer++;
+                    System.out.println("Верно, возвразаемся к примерам");
+                    System.out.println();
+
+                }
+                if (answerRight >= 5) {
+                    break;
+                }
             }
-            if ((answerRight + answerWrong) % 5 == 0) {
-                System.out.println(" \n Правильные ответы = " + answerRight + "; Неправильные ответы =  " + answerWrong);
-                break;
-            }
+
 
         }
+        System.out.println("Итоговая статистика: Правильные ответы = " + answerRight + "; Неправильные ответы =  " + answerWrong + " Дополнительные вопросы = " + extraAnswer);
     }
+
 }
+
+
+
 
